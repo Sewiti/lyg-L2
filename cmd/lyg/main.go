c29e8fd06edf62d3a6e90a103b7fffd97c8ccdf1
Mindaugas <bernotas.min@gmail.com>
2020-11-06 21:58:22 +0200

    Data thread

--- cmd/lyg/main.go
+++ cmd/lyg/main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"os"
+
+	"github.com/Sewiti/lyg-L2/internal/employee"
 )
 
 func main() {
-	fmt.Println("Hello world")
+	execute("data/data-1.json")
+}
+
+func execute(filename string) error {
+	employees, err := read(filename)
+	if err != nil {
+		return err
+	}
+
+	send := make(chan employee.Employee)
+	get := make(chan employee.Employee)
+
+	go data(send, get, len(employees))
+
+	for _, e := range employees {
+		send <- e
+	}
+	close(send)
+
+	return nil
+}
+
+func read(filename string) ([]employee.Employee, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var e []employee.Employee
+	err = json.NewDecoder(file).Decode(&e)
+	if err != nil {
+		return nil, err
+	}
+
+	return e, err
 }
--- internal/employee/employee.go
+++ internal/employee/employee.go
@@ -1,9 +1,15 @@
 package employee
 
+import "fmt"
+
 // Employee holds data about employee such as name, age, salary & computed hash
 type Employee struct {
 	Name   string  `json:"name"`
 	Age    int     `json:"age"`
 	Salary float64 `json:"salary"`
 	Hash   string
+}
+
+func (e *Employee) String() string {
+	return fmt.Sprintf("%s %d %f %s", e.Name, e.Age, e.Salary, e.Hash)
 }
